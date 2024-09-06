d6ee7d84f3972781a60dc3fa234f2492698f17fc
Vedant <vedantfutane2003@gmail.com>
2024-09-06 08:21:34 +0530

    Code Commited

---
+++ Stack_codes/Stack_Reverse.java
@@ -0,0 +1,36 @@
+package Stack_codes;
+import java.util.*;
+class Stack_Reverse{
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top= s.pop();
+        reverseStack(s);
+        pushAtBottom(s,top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+
+        System.out.println(s);
+        reverseStack(s);
+        System.out.println(s);
+    }
+}
