18088b94b8d3dbf87413a1b0887649cc045dca35
Vedant <vedantfutane2003@gmail.com>
2024-09-21 09:39:14 +0530

    Code commited

--- Queue_Codes/FirstNonRepeatLetter.java
+++ Queue_Codes/FirstNonRepeatLetter.java
@@ -9,7 +9,7 @@ public class FirstNonRepeatLetter {
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
             q.add(ch);
-            freq[ch-'a']++;
+            freq[ch-'a']++;  // increment in frequency
 
             while(!q.isEmpty() && freq[q.peek()-'a']>1){
                 q.remove();
